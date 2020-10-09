7b1d63a9cbab3feded838a8d2bcc60dcf8ac3880
saumyawiprogit <72594075+saumyawiprogit@users.noreply.github.com>
2020-10-09 13:03:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
 
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
+ 
+
+provider "aws" {
+ region = "${var.region}"
+ access_key = "${var.access_key}"
+secret_key = "${var.secret_key}"
+}
+
+ 
+
+resource "null_resource" "stop_instance2" {
+
+ 
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+        aws s3 ls
+        echo "***************************************Rebooted****************************************************"
+     EOT
+}
+ triggers = {
+    always_run = "${timestamp()}"
+  }
+}
