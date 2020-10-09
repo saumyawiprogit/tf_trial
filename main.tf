7900d1ac0136611f69c211539e53171a6df97591
saumyawiprogit <72594075+saumyawiprogit@users.noreply.github.com>
2020-10-09 17:37:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ variable "secret_key" {}
 
 
 provider "aws" {
- region = "${var.region}"
- access_key = "${var.access_key}"
-secret_key = "${var.secret_key}"
+ region = var.region
+ access_key = var.access_key
+secret_key = var.secret_key
 }
 
  
