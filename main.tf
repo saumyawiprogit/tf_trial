adbfd4d389935be779a9b4d966d3b9cf9c3a058c
saumyawiprogit <72594075+saumyawiprogit@users.noreply.github.com>
2020-10-12 11:24:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ variable "secret_key" {}
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.9.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
