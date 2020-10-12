e4518511fa4ec64affcdb63cf5373b4123650ea5
saumyawiprogit <72594075+saumyawiprogit@users.noreply.github.com>
2020-10-12 18:53:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
-variable "region" {}
-variable "access_key" {}
-variable "secret_key" {}
-
-  
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
 
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
 
-
+ 
 provider "aws" {
  region = var.region
  access_key = var.access_key
